35103c8d2299f32d49cfd6fbe93830f40cf26556
ajaxkulkarni <ajinkyashiva@gmail.com>
2016-11-20 20:47:58 +0530

    latest changes

---
+++ job-portal/src/main/java/com/rns/web/jobz/service/bo/domain/AdminMail.java
@@ -0,0 +1,28 @@
+package com.rns.web.jobz.service.bo.domain;
+
+public class AdminMail {
+	
+	private String subject;
+	private String message;
+	private String type;
+	
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
